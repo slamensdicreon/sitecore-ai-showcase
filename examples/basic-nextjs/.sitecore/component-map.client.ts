ace3e0242fb11c923d1d3e961dcab2d510201c6e
slamensdicreon <steve@icreon.com>
2026-03-26 06:29:55 +0000

    fix(task-34): update component maps - move server components out of client map

--- examples/basic-nextjs/.sitecore/component-map.client.ts
+++ examples/basic-nextjs/.sitecore/component-map.client.ts
@@ -3,23 +3,14 @@
 import { BYOCClientWrapper, NextjsContentSdkComponent, FEaaSClientWrapper } from '@sitecore-content-sdk/nextjs';
 import { Form } from '@sitecore-content-sdk/nextjs';
 
-import * as MegaTrends from 'src/components/mega-trends/MegaTrends';
 import * as ProductDiscovery from 'src/components/product-discovery/ProductDiscovery';
-import * as ProofPointCounter from 'src/components/proof-point-counter/ProofPointCounter';
-import * as CrossNavigation from 'src/components/cross-navigation/CrossNavigation';
 
 export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['BYOCWrapper', BYOCClientWrapper],
   ['FEaaSWrapper', FEaaSClientWrapper],
   ['Form', Form],
-  ['MegaTrends', { ...MegaTrends }],
-  ['Mega Trends', { ...MegaTrends }],
   ['ProductDiscovery', { ...ProductDiscovery }],
   ['Product Discovery', { ...ProductDiscovery }],
-  ['ProofPointCounter', { ...ProofPointCounter }],
-  ['Proof Point Counter', { ...ProofPointCounter }],
-  ['CrossNavigation', { ...CrossNavigation }],
-  ['Cross Navigation', { ...CrossNavigation }],
 ]);
 
 export default componentMap;
